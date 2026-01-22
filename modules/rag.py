cc4fea916fda3df170c13d936cd96ca2ecd715a5
Luano Cesar <luanocesar@gmail.com>
2026-01-22 08:21:27 -0400

    feat: update Embedding API on RAG

--- modules/rag.py
+++ modules/rag.py
@@ -1,12 +1,12 @@
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 class setup:
     def __init__(self,model_name:str, documents):
         self.model_name = model_name
         self.documents = documents
         
-        self.embeddings = HuggingFaceEmbeddings(model_name=model_name, model_kwargs={'device' : 'cpu'})
+        self.embeddings = GoogleGenerativeAIEmbeddings(model_name="models/text-embedding-004")
         
         self.vectorstore = FAISS.from_documents(documents, self.embeddings)
         
